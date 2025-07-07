1a2961d4f844cb1953afd7ab54c25e36393c5ae2
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-07-08 00:56:15 +0100

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,1 +1,13 @@
+// pages/Dashboard.jsx
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
+export default function Dashboard() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Dashboard Content for signup</h1>
+      <Footer />
+    </div>
+  );
+}
