28ea05e58261bd890a09d16814d62f2e5edd9860
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-07-08 00:50:31 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,15 @@
+// App.jsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />   // Not login.html!
+        <Route path="/signup" element={<Signup />} /> // Not signup.html!
+      </Routes>
+    </BrowserRouter>
+  );
+}
