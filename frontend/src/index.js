cf7f466b90f9f43eb6dd9d9defb66615a20d57db
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-07-08 00:40:37 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,12 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom/client'; 
+import App from './App'; // Note the curly braces
+import './App.css';
 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
