6c9a8e8b3f89d01887da3f7dc931db7e2c618a7a
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-07-08 01:14:49 +0100

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,12 +2,10 @@
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
-export default function Dashboard() {
+export default function Login() {
   return (
     <div>
-      <Navbar />
       <h1>Dashboard Content for login</h1>
-      <Footer />
     </div>
   );
 }
