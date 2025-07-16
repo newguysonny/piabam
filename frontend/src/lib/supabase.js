b61c0bb56925f48291d7bcd7c2ec492e88eaa16f
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-07-16 02:56:02 +0100

    Update supabase.js

--- frontend/src/lib/supabase.js
+++ frontend/src/lib/supabase.js
@@ -1,7 +1,7 @@
 // lib/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = vzdufzptwcuytmjhpfwr.supabase.co;
+const supabaseUrl = https://vzdufzptwcuytmjhpfwr.supabase.co;
 const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6ZHVmenB0d2N1eXRtamhwZndyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyMjUyNjIsImV4cCI6MjA2NTgwMTI2Mn0.DAwkFXtptkWVRqlYQi6Y1uiagqqW35MaPNP19rVhvOk;
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
