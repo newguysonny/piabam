c527875de0069e61faeebbbf1b20b7e9bbbd632f
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-07-22 19:14:13 +0100

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,75 @@
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
+import ProjectsSlider from '../../components/projects/ProjectsSlider';
+
+
+   const featuredProjects = [
+  // Your featured projects data
+  {
+    id: 1,
+    title: "Wizkid Album Listening Party",
+    host: "Atin's Fan Club",
+    type: "music",
+    image: "https://placehold.co/600x400/7E22CE/FFFFFF?text=Wizkid+Party",
+    location: "Lagos, Nigeria",
+    goal: 5000,
+    current: 3614,
+  },
+       {
+    id: 1,
+    title: "Wizkid Album Listening Party",
+    host: "'s Fan Club",
+    type: "music",
+    image: "https://placehold.co/600x400/7E22CE/FFFFFF?text=Wizkid+Party",
+    location: "Lagos, Nigeria",
+    goal: 5000,
+    current: 3614,
+  },
+       {
+    id: 1,
+    title: "Wizkid Album Listening Party",
+    host: "tin's Fan Club",
+    type: "music",
+    image: "https://placehold.co/600x400/7E22CE/FFFFFF?text=Wizkid+Party",
+    location: "Lagos, Nigeria",
+    goal: 5000,
+    current: 3614,
+  },
+       {
+    id: 1,
+    title: "Wizkid Album Listening Party",
+    host: "As Fan Club",
+    type: "music",
+    image: "https://placehold.co/600x400/7E22CE/FFFFFF?text=Wizkid+Party",
+    location: "Lagos, Nigeria",
+    goal: 5000,
+    current: 3614,
+  },
+       {
+    id: 1,
+    title: "Wizkid Album Listening Party",
+    host: "Ati's Fan Club",
+    type: "music",
+    image: "https://placehold.co/600x400/7E22CE/FFFFFF?text=Wizkid+Party",
+    location: "Lagos, Nigeria",
+    goal: 5000,
+    current: 3614,
+  },
+   
+];
+
+
 
 export default function HomePage() {
   return (
     <>
      <Navbar/>
+      <section className="py-12">
+      <div className="container mx-auto px-4">
+        <h2 className="text-2xl font-bold mb-8">Featured Community Projects</h2>
+        <ProjectsSlider projects={featuredProjects} />
+      </div>
+    </section>
       {/* Page content */}
       <Footer />
     </>
