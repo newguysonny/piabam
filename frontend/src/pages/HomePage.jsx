65ddde6365ac63a828d5bc7c6235a2a260ed8eb4
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-07-24 17:16:12 +0100

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import ReviewsCarousel from '../components/lem/ReviewsCarousel';
 import ProjectsSlider from '../components/projects/ProjectsSlider';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
