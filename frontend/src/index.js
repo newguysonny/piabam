da2089ae435d7c6ed04f531b30e06d318a1e92eb
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-07-30 17:54:09 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; 
 import App from './App'; // Note the curly braces
 import './App.css';
-
+import "leaflet/dist/leaflet.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
