80970f998d3c7ebdbf7c3529ba06f567d78807e3
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-07-30 17:59:48 +0100

    Create CrewMap.jsx

---
+++ frontend/src/pages/lem/CrewMap.jsx
@@ -0,0 +1,9 @@
+import MapWrapper from "../../components/lem/MapWrapper";
+
+export default function CrewMap() {
+  return (
+    <div className="w-full h-screen">
+      <MapWrapper />
+    </div>
+  );
+}￼Enter
