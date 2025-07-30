df415ffa4f11c4fbc89f2203f60a8ecdc56a0617
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-07-30 17:57:01 +0100

    Create MapWrapper.jsx

---
+++ frontend/src/components/lem/MapWrapper.jsx
@@ -0,0 +1,24 @@
+// src/components/lem/MapWrapper.jsx
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+export default function MapWrapper({ children }) {
+  const defaultPosition = [6.5244, 3.3792]; // Lagos
+
+  return (
+    <div className="w-full h-screen">
+      <MapContainer
+        center={defaultPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className="w-full h-full z-0"
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {children}
+      </MapContainer>
+    </div>
+  );
+}
