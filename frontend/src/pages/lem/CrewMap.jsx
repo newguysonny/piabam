4484b825a25c25d81444c51c03f9b72a1cf12b70
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-01 00:56:52 +0100

    Update CrewMap.jsx

--- frontend/src/pages/lem/CrewMap.jsx
+++ frontend/src/pages/lem/CrewMap.jsx
@@ -63,8 +63,9 @@ export default function CrewMap() {
           <MapFilter onClose={() => setShowFilter(false)} />
         </>
       )}
+      <Navbar />
     </div>
-    <Navbar />
+    
       
   );
 }
