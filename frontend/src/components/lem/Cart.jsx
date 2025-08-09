5ed587473f5b6cc756e30dbfd76aeab1423a1932
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-09 13:07:57 +0100

    Update Cart.jsx

--- frontend/src/components/lem/Cart.jsx
+++ frontend/src/components/lem/Cart.jsx
@@ -126,7 +126,7 @@ const Cart = () => {
             </button>
           </div>
         </div>
-      {/* Add Items Button */}
+      
       <div className="mt-4 flex justify-end">
         <button className="flex items-center bg-gray-100 px-3 py-1 rounded-full hover:bg-gray-200 transition">
           <AiOutlinePlus size={16} className="mr-1" /> Add items
