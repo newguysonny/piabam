d8d014053d03787f8d66fa64994c97410ef42c79
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-09 22:11:33 +0100

    Update Checkout.jsx

--- frontend/src/components/lem/Checkout.jsx
+++ frontend/src/components/lem/Checkout.jsx
@@ -1,12 +1,14 @@
 // components/Checkout.jsx
+import { useNavigate } from "react-router-dom";
 import Cart from "./Cart.jsx"
 
 export default function Checkout({ subtotal, escrowFee, discount }) {
+  const navigate = useNavigate();
   const total = subtotal + escrowFee - discount;
   const isEditable = true;
   return (
     <div className="bg-white rounded-t-2xl shadow-lg p-4 w-full max-w-md mx-auto">
-      
+      <h1 className="text-xl font-bold">Checkout</h1>
       <Cart isEditable={true} />
       {/* Subtotal */}
       <div className="flex justify-between text-gray-700 mb-2">
@@ -38,7 +40,8 @@ export default function Checkout({ subtotal, escrowFee, discount }) {
       </div>
 
       {/* Button */}
-      <button className="w-full bg-green-500 text-white rounded-lg py-3 font-semibold hover:bg-green-600 transition">
+      <buttonon Click={() => navigate("/payment-method")}
+      className="w-full bg-green-500 text-white rounded-lg py-3 font-semibold hover:bg-green-600 transition">
         Confirm order
       </button>
     </div>
