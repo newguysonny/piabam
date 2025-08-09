b26b14aa76f44fc40a2948df86f5b7fd97be891d
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-09 21:41:14 +0100

    Create PaymentMethod.jsx

---
+++ frontend/src/components/lem/PaymentMethod.jsx
@@ -0,0 +1,8 @@
+export default function PaymentMethod() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Select Payment Method</h1>
+      {/* Payment options here */}
+    </div>
+  );
+}
