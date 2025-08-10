fe6b91d2ed291a93d58851ee9a9650aa92f97909
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-10 22:48:03 +0100

    Update CrewMap.jsx

--- frontend/src/pages/lem/CrewMap.jsx
+++ frontend/src/pages/lem/CrewMap.jsx
@@ -52,15 +52,6 @@ export default function CrewMap() {
           />
         ))}
       </MapWrapper>
-
-        {/* Filter Button */}
-      <button
-        className="absolute top-4 right-4 z-40 bg-white rounded-full shadow-md p-2 hover:bg-gray-100 transition"
-        title="Filter"
-        onClick={() => setShowFilter(true)}
-      >
-        <SlidersHorizontal className="w-5 h-5 text-gray-700" />
-      </button>
       </div>
 
       {/* Filter Button */}
