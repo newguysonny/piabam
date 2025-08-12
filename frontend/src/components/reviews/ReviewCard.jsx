8f3c2e7fd425b1f299f025603b3e52e9a9fb2e26
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-12 16:28:34 +0100

    Create ReviewCard.jsx

---
+++ frontend/src/components/reviews/ReviewCard.jsx
@@ -0,0 +1,29 @@
+import ReviewerInfo from "./ReviewerInfo";
+import ReviewRating from "./ReviewRating";
+import ReviewPhotos from "./ReviewPhotos";
+import ReviewActions from "./ReviewActions";
+
+export default function ReviewCard({ review }) {
+  return (
+    <div className="border rounded-lg p-4 bg-white shadow-sm">
+      {/* Reviewer details */}
+      <ReviewerInfo user={review.user} date={review.date} />
+
+      {/* Rating */}
+      <ReviewRating rating={review.rating} />
+
+      {/* Text */}
+      <p className="mt-2 text-gray-700 text-sm leading-relaxed">
+        {review.text} <span className="text-blue-600 cursor-pointer">read more</span>
+      </p>
+
+      {/* Photos */}
+      {review.photos?.length > 0 && (
+        <ReviewPhotos photos={review.photos} />
+      )}
+
+      {/* Actions */}
+      <ReviewActions reactions={review.reactions} />
+    </div>
+  );
+}
