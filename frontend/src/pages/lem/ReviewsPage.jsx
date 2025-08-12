8a7f8de295b10c4f4bc7181c8083ba6710033d68
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-12 18:12:01 +0100

    Update ReviewsPage.jsx

--- frontend/src/pages/lem/ReviewsPage.jsx
+++ frontend/src/pages/lem/ReviewsPage.jsx
@@ -303,7 +303,7 @@ export default function ReviewsPage() {
     </div>
   );
   
- /* 
+ /** 
   return (
     <div className="max-w-3xl mx-auto px-4 py-6">
       <h1 className="text-2xl font-bold mb-6">Customer Reviews</h1>
