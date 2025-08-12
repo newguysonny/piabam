927a9facfa0e420eb3740886b8342cdd00e790ec
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-12 21:14:45 +0100

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -75,27 +75,6 @@ import ReviewCard from '../components/lem/ReviewCard';
 ];
 
 
-/* mockReviews
-const reviews = [
-  {
-    id: 1,
-    imageUrl: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092",
-    userAvatar: "https://placehold.co/400x225/EEE/31343C?text=profile+photo",
-    userName: "Audrey L.",
-    crewName: "Mr P Party",
-    reviewText: "Craving a delicious Mediterranean spot in the heart of the Richmond..."
-  },
-  {
-    id: 2,
-    imageUrl: "https://placehold.co/400x225/EEE/31343C?text=Smart+Widget",
-    userAvatar: "https://placehold.co/400x225/EEE/31343C?text=Smart+Widget",
-    userName: "Hannah F.",
-    crewName: "Tomiwa and Friends",
-    reviewText: "Just what this neighborhood was missing and BYOB - We tried takeout..."
-  },
-  // ...more
-];
-*/
 
 const reviews = [
   {
@@ -261,7 +240,7 @@ const reviews = [
   to="/review"
   className=" text-gray-600 underline inline-flex items-center hover:text-gray-800 transition"
 >
-  View all projects <span className="ml-1">→</span>
+  more <span className="ml-1">→</span>
   </Link>
      </div >      
     
@@ -275,86 +254,3 @@ const reviews = [
   );
 }
 
-
-
-/*
-import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
-import ProjectsSlider from '../components/projects/ProjectsSlider';
-
-
-   const featuredProjects = [
-  // Your featured projects data
-  {
-    id: 1,
-    title: "Wizkid Album Listening Party",
-    host: "Atin's Fan Club",
-    type: "music",
-    image: "https://placehold.co/600x400/7E22CE/FFFFFF?text=Wizkid+Party",
-    location: "Lagos, Nigeria",
-    goal: 5000,
-    current: 3614,
-  },
-       {
-    id: 2,
-    title: "Wizkid Album Listening Party",
-    host: "'s Fan Club",
-    type: "music",
-    image: "https://placehold.co/600x400/7E22CE/FFFFFF?text=Wizkid+Party",
-    location: "Lagos, Nigeria",
-    goal: 5000,
-    current: 3614,
-  },
-       {
-    id: 3,
-    title: "Wizkid Album Listening Party",
-    host: "tin's Fan Club",
-    type: "music",
-    image: "https://placehold.co/600x400/7E22CE/FFFFFF?text=Wizkid+Party",
-    location: "Lagos, Nigeria",
-    goal: 5000,
-    current: 3614,
-  },
-       {
-    id: 4,
-    title: "Wizkid Album Listening Party",
-    host: "As Fan Club",
-    type: "music",
-    image: "https://placehold.co/600x400/7E22CE/FFFFFF?text=Wizkid+Party",
-    location: "Lagos, Nigeria",
-    goal: 5000,
-    current: 3614,
-  },
-       {
-    id: 5,
-    title: "Wizkid Album Listening Party",
-    host: "Ati's Fan Club",
-    type: "music",
-    image: "https://placehold.co/600x400/7E22CE/FFFFFF?text=Wizkid+Party",
-    location: "Lagos, Nigeria",
-    goal: 5000,
-    current: 3614,
-  },
-   
-];
-
-
-
-export default function HomePage() {
-  return (
-    <>
-     <Navbar/>
-      <section className="py-12">
-      <div className="container mx-auto px-4">
-        <h2 className="text-2xl font-bold mb-8">Featured Community Projects</h2>
-        <div style={{ minHeight: '500px' }}>
-          <ProjectsSlider projects={featuredProjects} />
-         </div> 
-      </div>
-    </section>
-      {/* Page content /}
-      <Footer />
-    </>
-  );
-}
-*/
