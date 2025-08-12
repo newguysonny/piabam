7f51bdc87144fcfabe8efaade5fef39533a07c4c
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-12 16:32:36 +0100

    Create ReviewPhotos.jsx

---
+++ frontend/src/components/reviews/ReviewPhotos.jsx
@@ -0,0 +1,14 @@
+export default function ReviewPhotos({ photos }) {
+  return (
+    <div className="flex space-x-2 mt-3">
+      {photos.slice(0, 4).map((photo, idx) => (
+        <img
+          key={idx}
+          src={photo}
+          alt={`Review photo ${idx + 1}`}
+          className="w-20 h-20 object-cover rounded-md cursor-pointer"
+        />
+      ))}
+    </div>
+  );
+}
