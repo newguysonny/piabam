c5330f6bb8c5cfcb19c19c5c62c6212e6023794f
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-13 13:29:30 +0100

    Update and rename LocationDisplay.jsx to ProjectLocation.jsx

--- frontend/src/components/projects/ProjectLocation.jsx
+++ frontend/src/components/projects/ProjectLocation.jsx
@@ -1,11 +1,11 @@
 // components/LocationDisplay.jsx
 import { FiMapPin } from "react-icons/fi";
 
-const LocationDisplay = ({ location }) => (
+const ProjectLocation = ({ location }) => (
   <div className="flex items-center text-xs text-gray-500 mb-3">
     <FiMapPin size={12} className="mr-1" />
     {location}
   </div>
 );
 
-export default LocationDisplay;
+export default ProjectLocation;
