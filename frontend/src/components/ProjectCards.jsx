80d748281f277f8cb8e137f8e8829062fe6277a4
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-13 12:12:58 +0100

    Create ProjectCards.jsx

---
+++ frontend/src/components/ProjectCards.jsx
@@ -0,0 +1,35 @@
+import ProjectImage from "./ProjectImage";
+import ProjectProgress from "./ProjectProgress";
+import ProjectInfo from "./ProjectInfo";
+import ProjectStats from "./ProjectStats";
+import ProjectCTA from "./ProjectCTA";
+
+const ProjectCards = ({ project, categories, getCategoryIcon, calculateProgress }) => {
+  return (
+    <div className="bg-white rounded-xl overflow-hidden shadow-sm hover:shadow-md transition-shadow border border-gray-100">
+      <ProjectImage
+        image={project.image}
+        title={project.title}
+        type={project.type}
+        categories={categories}
+        getCategoryIcon={getCategoryIcon}
+      />
+
+      <div className="p-4">
+        <ProjectProgress
+          current={project.current}
+          goal={project.goal}
+          calculateProgress={calculateProgress}
+        />
+
+        <ProjectInfo project={project} />
+
+        <ProjectStats supporters={project.supporters} daysLeft={project.daysLeft} />
+
+        <ProjectCTA />
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCards;
