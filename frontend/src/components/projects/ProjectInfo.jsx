a0ca4a22ef616ab8289aa0d1ac0d5bc4eaf80c75
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-13 14:20:29 +0100

    Update ProjectInfo.jsx

--- frontend/src/components/projects/ProjectInfo.jsx
+++ frontend/src/components/projects/ProjectInfo.jsx
@@ -1,21 +1,13 @@
 import { FiMapPin } from "react-icons/fi";
 
-/*
+
 const ProjectInfo = ({ title, host, description }) => (
   <>
-    <h3 className="font-bold text-lg mb-1 line-clamp-1">{project.title}</h3>
-    <p className="text-gray-600 text-sm mb-1">Hosted by {project.host}</p>
-    <p className="text-gray-500 text-xs line-clamp-2 mb-2">{project.description}</p>
+    <h3 className="font-bold text-lg mb-1 line-clamp-1">{title}</h3>
+    <p className="text-gray-600 text-sm mb-1">Hosted by {host}</p>
+    <p className="text-gray-500 text-xs line-clamp-2 mb-2">{description}</p>
   </>
 );
-*/
 
-const ProjectInfo = ({ project }) => (
-  <>
-    <h3 className="font-bold text-lg mb-1 line-clamp-1">{project.title}</h3>
-    <p className="text-gray-600 text-sm mb-1">Hosted by {project.host}</p>
-    <p className="text-gray-500 text-xs line-clamp-2 mb-2">{project.description}</p>
-  </>
-);
 
 export default ProjectInfo;
