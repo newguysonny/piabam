8ba2c4b934848557519b9ad590070d64d055b706
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-14 01:13:25 +0100

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -331,6 +331,11 @@ const reviews = [
        <div className="max-w-4xl mx-auto p-4">
    <WeeklyCard title="New on Lem" items={shows} />
    </div>
+
+        {/* Weekly projects ending and launching*/}
+        <div className="max-w-6xl mx-auto p-4">
+   <WeeklyProjects title="This Week's Projects" projects={featuredProjects} />
+   </div>
         
       <Footer />
     </>
