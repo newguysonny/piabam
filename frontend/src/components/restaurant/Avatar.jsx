196329cb0cf85b50cefb25a6d5d6b9c5edd80900
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-17 14:32:15 +0100

    Create Avatar.jsx

---
+++ frontend/src/components/restaurant/Avatar.jsx
@@ -0,0 +1,12 @@
+‎/*  Avatar Component */
+‎function Avatar({ src }) {
+‎  return (
+‎    <div className="absolute left-1/2 -bottom-8 transform -translate-x-1/2">
+‎      <img
+‎        src={src}
+‎        alt="Restaurant avatar"
+‎        className="w-20 h-20 rounded-full border-4 border-white shadow-md"
+‎      />
+‎    </div>
+‎  );
+‎}
