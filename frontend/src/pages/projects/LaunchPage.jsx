c517441972ee9a24528a54b522ebd6e4d2f8151e
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-17 04:20:01 +0100

    Update LaunchPage.jsx

--- frontend/src/pages/projects/LaunchPage.jsx
+++ frontend/src/pages/projects/LaunchPage.jsx
@@ -3,9 +3,13 @@ import HeaderSection from "../../components/projects/HeaderSection";
 
 function LaunchPage() {
   return (
+    <>
     <div className=" mt-6 flex items-center justify-center bg-white">
       <Countdown targetDate="2025-08-20T12:00:00" />
+      
     </div>
+    <HeaderSection />
+   </>
   );
 }
 
