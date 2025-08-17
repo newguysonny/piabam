8ad7e45dc26548c5347bf61c0c6be63e93f2d70d
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-17 03:37:43 +0100

    Update LaunchPage.jsx

--- frontend/src/pages/projects/LaunchPage.jsx
+++ frontend/src/pages/projects/LaunchPage.jsx
@@ -2,7 +2,7 @@ import Countdown from "../../components/projects/Countdown";
 
 function LaunchPage() {
   return (
-    <div className="h-screen flex items-center justify-center bg-white">
+    <div className=" flex items-center justify-center bg-white">
       <Countdown targetDate="2025-08-20T12:00:00" />
     </div>
   );
