92b63c6408734e4704c42fdae546452d3195b4a2
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-17 14:26:16 +0100

    Create CoverImage.jsx

---
+++ frontend/src/components/restaurant/CoverImage.jsx
@@ -0,0 +1,27 @@
+‎/*  Cover Image Component */
+‎export default function CoverImage({ src }) {
+‎  return (
+‎    <div className="relative">
+‎      <img
+‎        src={src}
+‎        alt="Restaurant cover"
+‎        className="w-full h-56 object-cover"
+‎      />
+‎
+‎      {/* Top action buttons */}
+‎      <div className="absolute top-3 left-3 flex space-x-2">
+‎        <button className="bg-black/50 text-white p-2 rounded-full">
+‎          <FaArrowLeft />
+‎        </button>
+‎      </div>
+‎      <div className="absolute top-3 right-3 flex space-x-2">
+‎        <button className="bg-black/50 text-white p-2 rounded-full">
+‎          <FaHeart />
+‎        </button>
+‎        <button className="bg-black/50 text-white p-2 rounded-full">
+‎          <FaEllipsisH />
+‎        </button>
+‎      </div>
+‎    </div>
+‎  );
+‎}
