4e636252ac00df533f199529b58b252c7fdd13ea
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-19 01:45:26 +0100

    Update CrewPreview.jsx

--- frontend/src/components/lem/CrewPreview.jsx
+++ frontend/src/components/lem/CrewPreview.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Cart from "./CartPreview.jsx";
+import CartPreview from "./CartPreview.jsx";
 
 
 const crewCart = {
