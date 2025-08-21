daec0f9a2f66f1ed922df4e1a9e3b1c0855ae3a6
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-21 02:23:17 +0100

    Update LaunchPage.jsx

--- frontend/src/pages/projects/LaunchPage.jsx
+++ frontend/src/pages/projects/LaunchPage.jsx
@@ -1,8 +1,14 @@
+import Countdown from "../../components/projects/Countdown";
+import HeaderSection from "../../components/projects/HeaderSection";
+import CallToAction from "../../components/projects/CallToAction";
+import BackItFooter from "../../components/projects/BackItFooter";
+
+
 function LaunchPage() {
   return (
     <>
       <div className="mt-6 flex items-center justify-center bg-white">
-        <Countdown targetDate="2025-08-20T12:00:00" />
+        <Countdown targetDate="2025-08-22T12:00:00" />
       </div>
 
       <div className="max-w-md mx-auto bg-gray-900 text-white p-8 rounded-lg shadow-xl pb-32">
@@ -15,7 +21,7 @@ function LaunchPage() {
     </>
   );
 }
-
+export default LaunchPage;
 
 /*
 import Countdown from "../../components/projects/Countdown";
