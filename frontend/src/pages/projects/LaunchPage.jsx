2e3e5272f5c8a8a50277fc8488229f691fa302b6
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-21 01:51:22 +0100

    Update LaunchPage.jsx

--- frontend/src/pages/projects/LaunchPage.jsx
+++ frontend/src/pages/projects/LaunchPage.jsx
@@ -1,6 +1,7 @@
 import Countdown from "../../components/projects/Countdown";
 import HeaderSection from "../../components/projects/HeaderSection";
 import CallToAction from "../../components/projects/CallToAction";
+import BackItFooter from "../../components/projects/BackItFooter";
 
 function LaunchPage() {
   return (
@@ -12,6 +13,7 @@ function LaunchPage() {
      <div className="max-w-md mx-auto bg-gray-900 text-white p-8 rounded-lg shadow-xl">
       <HeaderSection />
       <CallToAction />
+      <BackItFooter endDate="Aug 15, 2025 at 9:59pm WAST." />
     </div>
    </>
   );
