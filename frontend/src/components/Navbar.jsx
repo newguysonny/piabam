d155a99d0caa822f6a4d9fcbeddc9d97a93c6477
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-22 16:19:45 +0100

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from '../context/AuthContext';
 import { throttle } from 'lodash';
 import { 
   FiSearch, 
