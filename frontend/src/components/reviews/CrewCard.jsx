13443ad4a4bde3f14de3467d9b63be9c62fff2aa
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-22 23:35:09 +0100

    Update CrewCard.jsx

--- frontend/src/components/reviews/CrewCard.jsx
+++ frontend/src/components/reviews/CrewCard.jsx
@@ -12,7 +12,7 @@ export default function CrewCard({ image, title="Lagos Foodie Club" }) {
 
       {/* Right side: Title +  Stats */}
       <div className="flex flex-col flex-1">
-        <h3 className="text-sm font-semibold text-purple-500">{title}</h3>
+        <h3 className="text-sm font-semibold text-purple-700">{title}</h3>
         <CrewStats />
       </div>
     </div>
