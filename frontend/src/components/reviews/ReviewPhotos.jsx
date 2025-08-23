b09aa9df766928a166ba3f874ca85cf8edcfe7fd
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-23 17:29:06 +0100

    Update ReviewPhotos.jsx

--- frontend/src/components/reviews/ReviewPhotos.jsx
+++ frontend/src/components/reviews/ReviewPhotos.jsx
@@ -1,4 +1,4 @@
-export default function ReviewPhotos({ photos }) {
+export default function ReviewPhotos({ photos, onPhotoClick }) {
   return (
     <div className="flex space-x-2 mt-3">
       {photos.slice(0, 4).map((photo, idx) => (
@@ -7,6 +7,7 @@ export default function ReviewPhotos({ photos }) {
           src={photo}
           alt={`Review photo ${idx + 1}`}
           className="w-20 h-20 object-cover rounded-md cursor-pointer"
+          onClick={() => onPhotoClick(idx)} //passed from the review card component for photo view 
         />
       ))}
     </div>
