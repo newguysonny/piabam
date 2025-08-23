dd54a0989fafe5c9400cf73f21577d04dbfd3bca
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-23 21:46:18 +0100

    Update CartPreview.jsx

--- frontend/src/components/lem/CartPreview.jsx
+++ frontend/src/components/lem/CartPreview.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react", 
+import { useState } from "react";
 import { FiTrash2, FiPlus, FiMinus } from "react-icons/fi";
 import { AiOutlinePlus } from "react-icons/ai";
 {/*
