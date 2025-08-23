68d6f4d7478b24466357e7ef6c98539cb2e0da08
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-23 04:36:04 +0100

    Update EscrowCard.jsx

--- frontend/src/components/escrow/EscrowCard.jsx
+++ frontend/src/components/escrow/EscrowCard.jsx
@@ -8,7 +8,7 @@ const EscrowCard = ({ transaction }) => {
   const handleViewDetails = () => {
     navigate(`/crew-room/${transaction.id}`, {
       state: { transactionData: transaction }
-    });
+    },{ state: { fromApp: true } });
   };
 
   return (
