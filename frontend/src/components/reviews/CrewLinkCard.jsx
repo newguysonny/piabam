01cf1cad5d163b56bac95a492b9bc755ea5dc19d
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-08-23 01:46:56 +0100

    Update CrewLinkCard.jsx

--- frontend/src/components/reviews/CrewLinkCard.jsx
+++ frontend/src/components/reviews/CrewLinkCard.jsx
@@ -1,25 +1,26 @@
 import CrewStats from "../crew/CrewStats";
+import { Link } from "react-router-dom";
 
-export default function CrewLinkCard({ image, crewId, title="Lagos Foodie Club" }) {
+export default function CrewLinkCard({ image, crewId, title = "Lagos Foodie Club" }) {
   return (
     <Link 
-‎      to={`/map?crew=${crewId}`} // This will open the modal on the map page
-‎      className="block hover:bg-gray-200 transition-colors"
-      >
-    <div className="flex items-center gap-3 p-3 bg-gray-100  rounded-lg shadow-md">
-      {/* Crew Image */}
-      <img
-        src={image}
-        alt={title}
-        className="w-20 h-14 rounded-md object-cover"
-      />
+      to={`/map?crew=${crewId}`}
+      className="block hover:bg-gray-200 transition-colors"
+    >
+      <div className="flex items-center gap-3 p-3 bg-gray-100 rounded-lg shadow-md">
+        {/* Crew Image */}
+        <img
+          src={image}
+          alt={title}
+          className="w-20 h-14 rounded-md object-cover"
+        />
 
-      {/* Right side: Title +  Stats */}
-      <div className="flex flex-col flex-1">
-        <h3 className="text-sm font-bold hover:text-purple-600 active:text-purple-500">{title}</h3>
-        <CrewStats />
+        {/* Right side: Title + Stats */}
+        <div className="flex flex-col flex-1">
+          <h3 className="text-sm font-bold hover:text-purple-600 active:text-purple-500">{title}</h3>
+          <CrewStats />
+        </div>
       </div>
-    </div>
     </Link>
   );
 }
